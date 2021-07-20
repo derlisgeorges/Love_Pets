3d51cfd9a2cc93c6531e906909b8057913dc9d48
vini <vinissfigueiroa@gmail.com>
2021-07-20 16:56:49 -0300

    interfaces feita

--- reforco/tarde/lovePets/sprint-2-back-end/senai_lovePets_webApi/senai_lovePets_webApi/Repositories/ClinicaRepository.cs
+++ reforco/tarde/lovePets/sprint-2-back-end/senai_lovePets_webApi/senai_lovePets_webApi/Repositories/ClinicaRepository.cs
@@ -1,11 +1,47 @@
-﻿using System;
+﻿using senai_lovePets_webApi.Context;
+using senai_lovePets_webApi.Domains;
+using senai_lovePets_webApi.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace senai_lovePets_webApi.Repositories
 {
-    public class ClinicaRepository
+    public class ClinicaRepository : IClinicaRepository
     {
+        /// <summary>
+        /// Instancia o objeto ctx com a classe lovePetsContext
+        /// </summary>
+        lovePetsContext ctx = new lovePetsContext();
+
+        public void Atualizar(int idClinica, Clinica ClinicaAtualizada)
+        {
+            
+        }
+
+        public Clinica BuscarPorId(int idClinica)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Clinica novaClinica)
+        {
+            ctx.Clinica.Add(novaClinica);
+
+            ctx.SaveChanges();
+        }
+
+        public void Deletar(int idClinica)
+        {
+            ctx.Clinica.Remove(BuscarPorId(idClinica));
+
+            ctx.SaveChanges();
+        }
+
+        public List<Clinica> ListarTodos()
+        {
+            return ctx.Clinicas.ToList();
+        }
     }
 }
